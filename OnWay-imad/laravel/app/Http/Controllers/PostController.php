1a73e6ced86b04f81152126fd3b48e2afce50e66
hakima elhani <hakimahanidev@gmail.com>
2024-01-01 23:19:14 +0100

    add PostController

---
+++ OnWay-imad/laravel/app/Http/Controllers/PostController.php
@@ -0,0 +1,71 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use App\Models\rc;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        return view('community.index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    } 
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+
+    public function show(Post $rc)
+    {
+        //
+
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+
+    public function edit(Post $rc)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+
+    public function update(Request $request, Post $rc)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+
+    public function destroy(Post $rc)
+    {
+        //
+    }
+}
